25d5eefbb9b369979e9e24052b1d09926bb82584
Loic Gouarin <loic.gouarin@gmail.com>
2024-12-09 14:32:52 +0100

    ...

--- tests/test_adapt.cpp
+++ tests/test_adapt.cpp
@@ -20,7 +20,7 @@ namespace samurai
 
     TYPED_TEST(adapt_test, mutliple_fields)
     {
-        ::samurai::initialize();
+        initialize();
 
         static constexpr std::size_t dim = TypeParam::value;
         using config                     = MRConfig<dim>;
@@ -31,6 +31,6 @@ namespace samurai
 
         auto adapt = make_MRAdapt(u_1, u_2, u_3);
         adapt(1e-4, 2);
-        ::samurai::finalize();
+        finalize();
     }
 }
